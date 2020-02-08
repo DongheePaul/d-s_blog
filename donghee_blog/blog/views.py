d9d59395a1ff02f3bf1f3493e7d7695cf6727bbe
Ubuntu <ubuntu@ip-172-26-0-180.ap-northeast-2.compute.internal>
2020-02-08 18:21:27 +0000

    template language applied

--- donghee_blog/blog/views.py
+++ donghee_blog/blog/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blog.models import Post
 
 def helloworld(request):
     return HttpResponse('hello world!')
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__isnull=False).order_by('-created_date')  # 수정된 부분
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/post_list.html', context)
