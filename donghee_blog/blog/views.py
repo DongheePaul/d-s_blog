f7f593649764bec014e57419022dd91d12e13751
Ubuntu <ubuntu@ip-172-26-0-180.ap-northeast-2.compute.internal>
2020-02-13 12:54:14 +0000

    user signup, login, logout

--- donghee_blog/blog/views.py
+++ donghee_blog/blog/views.py
@@ -1,10 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from blog.models import Post
-from django.contrib.auth import get_user_model
-from .form import PostForm
+from django.contrib.auth import get_user_model, views, models, login
+from django.views.generic import CreateView 
+from .form import PostForm, UserForm
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
 
+#회원가입. 
+def signup(request):
+    if request.method == "POST": #1
+        #유저가 입력한 비밀번호와 비밀번호 확인이 동일하다면
+        if request.POST["password1"] == request.POST["password2"]:
+            user = models.User.objects.create_user(
+                username = request.POST["username"], 
+                password = request.POST["password1"])
+            return redirect(post_list)
+        return render(request, 'blog/signup.html')
+    return render(request, 'blog/signup.html') #4
 
 def post_list(request):
     posts = Post.objects.filter(published_date__isnull=False).order_by('-created_date')  # 수정된 부분
@@ -20,6 +34,7 @@ def post_detail(request, pk):
     } 
     return render(request, 'blog/post_detail.html', context)
 
+@login_required
 def post_add(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -32,7 +47,8 @@ def post_add(request):
     else:
         form = PostForm()
         return render(request, 'blog/post_add.html', {'form': form})
-        
+
+@login_required
 def post_delete(request, pk):
     if request.method == 'POST':
         post = Post.objects.get(pk=pk)
@@ -42,7 +58,7 @@ def post_delete(request, pk):
     elif request.method == 'GET':
         return HttpResponse('잘못된 접근 입니다.')
 
-
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
--- donghee_blog/config/urls.py
+++ donghee_blog/config/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import post_list, post_detail, post_add, post_delete, post_edit
+from blog.views import post_list, post_detail, post_add, post_delete, post_edit, signup
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,10 @@ urlpatterns = [
     path('post/add/', post_add, name='post_add'),
     path('post/<int:pk>/delete', post_delete, name='post_delete'),
     path('post/<int:pk>/edit/', post_edit, name='post_edit'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name="login"),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('join/', signup, name='join'),
+
 ]
 
 urlpatterns += static(
