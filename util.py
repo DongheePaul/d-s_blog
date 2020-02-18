3a1a0692fcd2ab933fef25579298482d3608ebab
Ubuntu <ubuntu@ip-172-26-0-180.ap-northeast-2.compute.internal>
2020-02-18 11:43:45 +0000

    file dir update in util.py

--- util.py
+++ util.py
@@ -1,8 +1,11 @@
-import json
+import json, os
 
 
 def get_server_info_value(key: str):
-    with open('/srv/donghee_blog/server_info.json', mode='rt', encoding='utf-8') as file:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    FILE_ROOT = os.path.join(BASE_DIR, 'server_info.json') 
+
+    with open(FILE_ROOT, mode='rt', encoding='utf-8') as file:
         data = json.load(file)
         for k, v in data.items():
             if k == key:
