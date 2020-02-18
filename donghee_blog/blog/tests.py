ce8ca9a49ae43a664b859f85d4f9c13bec2a2d37
Ubuntu <ubuntu@ip-172-26-0-180.ap-northeast-2.compute.internal>
2020-02-18 05:03:46 +0000

    create travis.yml

--- donghee_blog/blog/tests.py
+++ donghee_blog/blog/tests.py
@@ -1,11 +1,1 @@
 from django.test import TestCase
-from django.test import Client
-from django.contrib.auth.models import User
-
-class AccountTest(TestCase):
-  # 로그인 테스트s./man
-  def test_login(self):
-    client = Client()
-    user = User.objects.create_user(username='donghee',password='1234')
-    response = client.post('/login',{'username':'donghee', 'password':'1234'})
-    self.assertEqual(response.status_code, 200) 
