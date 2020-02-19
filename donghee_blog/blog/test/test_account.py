77efda36e6e2e7b9a7e33c9f9830e220dc63e9dc
Ubuntu <ubuntu@ip-172-26-0-180.ap-northeast-2.compute.internal>
2020-02-19 05:04:31 +0000

    pytest setting and travis.yml update for pytest

--- donghee_blog/blog/test/test_account.py
+++ donghee_blog/blog/test/test_account.py
@@ -6,6 +6,6 @@ class AccountTest(TestCase):
   # 로그인 테스트s./man
   def test_login(self):
     client = Client()
-    user = User.objects.create_user(username='donghee',password='1234')
-    response = client.post('/login',{'username':'donghee', 'password':'1234'})
+    user = User.objects.create_user(username='donghee', password='1234')
+    response = client.post('accounts/login/',{'username':'donghee', 'password':'1234'})
     self.assertEqual(response.status_code, 200) 
