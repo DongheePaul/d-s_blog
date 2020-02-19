8e729b80240b6a4c3d2ee03aa00ef8c693ee41c8
Ubuntu <ubuntu@ip-172-26-0-180.ap-northeast-2.compute.internal>
2020-02-19 06:09:38 +0000

    travis updates for privilege of db

--- donghee_blog/blog/test/test_account.py
+++ donghee_blog/blog/test/test_account.py
@@ -7,5 +7,5 @@ class AccountTest(TestCase):
   def test_login(self):
     client = Client()
     user = User.objects.create_user(username='donghee', password='1234')
-    response = client.post('accounts/login/',{'username':'donghee', 'password':'1234'})
+    response = client.post('/accounts/login',{'username':'donghee', 'password':'1234'})
     self.assertEqual(response.status_code, 200) 
